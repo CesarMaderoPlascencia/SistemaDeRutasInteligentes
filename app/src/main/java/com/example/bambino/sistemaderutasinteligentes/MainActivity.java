8b2f1f1b9ce6cea34d950693bc4c20a09e84fff4
CesarMaderoPlascencia <cesarurmp@gmail.com>
2018-09-12 16:03:43 -0500

    Se incorpora OpenStreetMaps al proyecto

--- app/src/main/java/com/example/bambino/sistemaderutasinteligentes/MainActivity.java
+++ app/src/main/java/com/example/bambino/sistemaderutasinteligentes/MainActivity.java
@@ -1,13 +1,66 @@
 package com.example.bambino.sistemaderutasinteligentes;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.ActivityCompat;
+import android.os.Bundle;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.MapController;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.OverlayItem;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MapView myOpenMapView;
+    private MapController myMapController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (tengoPermisoEscritura()) {
+            mapaCargado();
+        }
+    }
+
+    private void mapaCargado()
+    {
+        GeoPoint madrid = new GeoPoint(40.416775, -3.70379);
+
+        myOpenMapView = (MapView) findViewById(R.id.openmapview);
+        myOpenMapView.setBuiltInZoomControls(true);
+        myMapController = (MapController) myOpenMapView.getController();
+        myMapController.setCenter(madrid);
+        myMapController.setZoom(60);
+
+        myOpenMapView.setMultiTouchControls(true);
+    }
+
+    public boolean tengoPermisoEscritura() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                return true;
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        } else {
+            return true;
+        }
     }
 }
